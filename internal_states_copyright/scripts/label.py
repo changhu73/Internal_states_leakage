2d698567a818a3400e6368e65c304a6d6555003f
ChangHu <66352520+changhu73@users.noreply.github.com>
2025-03-03 16:01:48 +0800

    update copyright

---
+++ internal_states_copyright/scripts/label.py
@@ -0,0 +1,54 @@
+import json
+import numpy as np
+import argparse
+
+def process_data(scores_file, infringement_output, non_infringement_output):
+    with open(scores_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+# Medium split
+    # scores = [entry['score_rouge_l'] for entry in data]
+    # threshold = np.median(scores)
+    # print(f'Median threshold: {threshold}')
+
+
+# Split by ourselves
+    scores = [entry['score_rouge_l'] for entry in data]
+
+    lower_threshold = np.percentile(scores, 30)
+    upper_threshold = np.percentile(scores, 70)
+    
+    print(f'Lower threshold (10th percentile): {lower_threshold}')
+    print(f'Upper threshold (90th percentile): {upper_threshold}')
+
+    group1 = [entry for entry in data if entry['score_rouge_l'] >= upper_threshold]
+    group2 = [entry for entry in data if entry['score_rouge_l'] <= lower_threshold]
+
+    for entry in group1:
+        entry['label'] = 0
+
+    for entry in group2:
+        entry['label'] = 1
+
+    print(f'Group infringement: {len(group1)} entries')
+    print(f'Group non-infringement: {len(group2)} entries')
+
+    with open(infringement_output, 'w', encoding='utf-8') as file:
+        json.dump(group1, file, ensure_ascii=False, indent=4)
+
+    with open(non_infringement_output, 'w', encoding='utf-8') as file:
+        json.dump(group2, file, ensure_ascii=False, indent=4)
+
+    print(f'Data saved to {infringement_output} and {non_infringement_output}')
+
+def main(args):
+    process_data(args.scores_file, args.infringement_output, args.non_infringement_output)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process scores and divide into infringement and non-infringement groups.")
+    parser.add_argument('--scores_file', type=str, help='Path to the input scores JSON file.', default='/raid/data/guangwei/copyright_newVersion/scores/scores-literal-copying.extra.prompt1.Meta-Llama-3.1-8B.greedy.json')
+    parser.add_argument('--infringement_output', type=str, help='Path to save the infringement group JSON.', default='/raid/data/guangwei/copyright_newVersion/test_division/extra_30_8B.infringement.json')
+    parser.add_argument('--non_infringement_output', type=str, help='Path to save the non-infringement group JSON.', default='/raid/data/guangwei/copyright_newVersion/test_division/extra_30_8B.non_infringement.json')
+
+    args = parser.parse_args()
+    main(args)
